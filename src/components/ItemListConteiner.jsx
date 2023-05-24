efe1ded28254a41ec9e46d210d1cb657e46e888b
Jesica Rodriguez <jesicarodriguez795@gmil.com>
2023-05-24 12:54:50 -0300

    primerapreentrega

---
+++ src/components/ItemListConteiner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ItemListConteiner = ({greeting}) => {
+    return (
+        <div style={{fontSize:20, color:"blue"}}>(greeting)</div>
+    )
+}
+
+export default ItemListConteiner
